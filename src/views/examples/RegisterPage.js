97a061d86ed8de2729f5ee20397a989605d40e03
AleksaOpacic <aleksaopacic988@gmail.com>
2020-03-24 02:39:56 +0100

    First commit

---
+++ src/views/examples/RegisterPage.js
@@ -0,0 +1,96 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+
+function RegisterPage() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("register-page");
+    return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
+  return (
+    <>
+      <ExamplesNavbar />
+      <div
+        className="page-header"
+        style={{
+          backgroundImage: "url(" + require("assets/img/login-image.jpg") + ")"
+        }}
+      >
+        <div className="filter" />
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" lg="6">
+              <Card className="card-register ml-auto mr-auto">
+                <h3 className="title mx-auto">Welcome</h3>
+                
+                <Form className="register-form">
+                  
+                  <div class="form-group">
+                      <input type="email" class="form-input" name="email" id="email" placeholder="Your Email"/>
+                  </div>
+                  <div class="form-group">
+                      <input type="text" class="form-input" name="password" id="password" placeholder="Password"/>
+                      <span toggle="#password" class="zmdi zmdi-eye field-icon toggle-password"></span>
+                  </div>
+                  <div class="form-group">
+                      <input type="password" class="form-input" name="re_password" id="re_password" placeholder="Repeat your password"/>
+                  </div>
+                  <div class="form-group">
+                      <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                      <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                  </div>
+                  <Button block className="form-submit" color="danger">
+                    Create account
+                  </Button>
+                </Form>
+                <div className="forgot">
+                  <Button
+                    className="btn-link"
+                    color="danger"
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                  >
+                    Forgot password?
+                  </Button>
+                </div>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        <div className="footer register-footer text-center">
+          <h6>
+            © {new Date().getFullYear()}, made with{" "}
+            <i className="fa fa-heart heart" /> by Creative Tim
+          </h6>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RegisterPage;
